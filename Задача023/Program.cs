d6c5a802514a21cb951aa0f30ae24a33ddd06179
Elena <bellenaelena@gmail.com>
2022-08-29 19:04:44 +0300

    добавлено решение задачи 023

---
+++ Задача023/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Таблица кубов ");
+for (int i = 1; i <= number; i++)
+{
+    Console.Write(Math.Pow(i,3)+ " ");
+}
