acfdc8c43d1735037fde99a0e0cc981739a29ac7
Elena <bellenaelena@gmail.com>
2022-08-30 14:57:52 +0300

    добавлено решение задачи 019

---
+++ Задача019/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Enter number: ");
+string? number = Console.ReadLine();
+
+void Number (string number)
+{
+if (number[0] == number [4] && number[1] == number[3])
+{
+    Console.WriteLine("Число является палиндромом!");
+}
+else
+{
+   Console.WriteLine("Число не является палиндромом!"); 
+}
+}
+
+if (number.Length == 5)
+{
+    Number(number);
+}
+else
+{
+    Console.Write("Введите пятизначное число");
+}
+
